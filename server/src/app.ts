eba09cd41af43d775c05f28b4983c15458ce73bc
MuktadirHassan <tamimmuktadir@gmail.com>
2023-09-02 14:17:22 +0600

    (core): add frontend stuff

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import { handleGlobalError } from "./utils/Error";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
